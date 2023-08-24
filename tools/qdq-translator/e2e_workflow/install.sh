dc6bd61a868ad9a895b682da29cfd64d8f13ddc5
Oliver Knieps <oknieps@nvidia.com>
2023-08-24 16:26:56 -0700

    Remove duplicated tensorflow_workflow files

--- tools/qdq-translator/e2e_workflow/install.sh
+++
@@ -1,7 +0,0 @@
-git clone ssh://git@gitlab-master.nvidia.com:12051/TensorRT/Tools/tensorflow-quantization.git
-cd tensorflow-quantization
-./install.sh
-cd examples
-pip install -r requirements.txt
-pip install --upgrade --force-reinstall protobuf==3.20.0
-cd ../..
